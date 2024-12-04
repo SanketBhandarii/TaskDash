32064362044f42a0a1462ccb28f104c8c52e6424
SanketBhandarii <bhandarisanketp@gmail.com>
2024-12-04 17:28:05 +0530

    Updates

--- src/pages/TaskDashboard.jsx
+++ src/pages/TaskDashboard.jsx
@@ -66,7 +66,7 @@ const TasksDashboard = () => {
 
         <FilterBar currentFilter={filter} setFilter={setFilter} />
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8">
           {tasks.length === 0 ? (
             <p className="text-center col-span-full text-xl text-gray-500">No tasks available</p>
           ) : (
